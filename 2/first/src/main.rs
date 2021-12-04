1aec6dec96c7bc4c3fc4f4f2f7934ca13afa9d19
brndd <burneddi@gmail.com>
2021-12-05 00:03:44 +0200

    Second day puzzles.

---
+++ 2/first/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let mut depth: i32 = 0;
+    let mut distance: i32 = 0;
+
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        if let Ok(text) = line {
+            let words: Vec<&str> = text.split(' ').collect();
+            let command: &str = words[0];
+            let value: i32;
+            match words[1].parse::<i32>() {
+                Ok(n) => value = n,
+                Err(_) => continue,
+            }
+
+            match command {
+                "forward" => distance += value,
+                "down" => depth += value,
+                "up" => depth -= value,
+                _ => continue,
+            }
+        }
+    }
+
+    println!("Depth: {} | Distance: {}", depth, distance);
+    println!("Solution: {}", depth * distance);
+}
